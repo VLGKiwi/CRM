7af34855aaa2c043a79c00dad1d530887cbb0236
VLGKiwi <zvonarev.dev@gmail.com>
2025-06-08 20:05:44 +0300

    save .next

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OkJnQ8/OQxbbgH22GUmAgl7htYsBy0DP6IFWXVmJyrQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
